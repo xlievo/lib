df6eec1b57af6351ae3396b4d06a80cdac4191d5
ievo <xlievo@live.com>
2019-01-25 05:38:22 +0800

    update: redis.sh docker rm -f redis

--- redis/redis.sh
+++ redis/redis.sh
@@ -2,6 +2,8 @@
 
 p=`openssl rand -base64 8 | cut -c 1-8`
 
-docker run -d --name redis --privileged=true --ulimit nproc=65535:65535 -p 5900:6379 daocloud.io/library/redis:latest redis-server --requirepass $p --maxclients 10000 --tcp-keepalive 60 --timeout 120
+sudo docker rm -f redis
+
+sudo docker run -d --name redis --privileged=true --ulimit nproc=65535:65535 -p 5900:6379 daocloud.io/library/redis:latest redis-server --requirepass $p --maxclients 10000 --tcp-keepalive 60 --timeout 120
 
 echo "port=5900 password=$p maxclients=10000 tcp-keepalive=60 timeout=120"
