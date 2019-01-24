8eb9bf8971c9e559525270982e4b59aaa9ae2266
ievo <xlievo@live.com>
2019-01-25 05:30:55 +0800

    update: postgres.sh mkdir /root/workspace/db

--- postgres/postgres.sh
+++ postgres/postgres.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo rm -rf /root/workspace/db && sudo mkdir -p -m=rwx /root/workspace/db
+
 p=`openssl rand -base64 8 | cut -c 1-8`
 
-docker run -d --name db -v /root/workspace/db:/var/lib/postgresql/data -e POSTGRES_USER=root -e POSTGRES_PASSWORD=$p -p 5920:5432 daocloud.io/library/postgres:latest -c 'shared_buffers=256MB' -c 'max_connections=500'
+sudo docker run -d --name db -v /root/workspace/db:/var/lib/postgresql/data -e POSTGRES_USER=root -e POSTGRES_PASSWORD=$p -p 5920:5432 daocloud.io/library/postgres:latest -c 'shared_buffers=256MB' -c 'max_connections=500'
 
 echo "port=5920 password=$p max_connections=500 shared_buffers=256MB data=/root/workspace/db"
