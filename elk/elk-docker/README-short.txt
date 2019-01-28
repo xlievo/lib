dac1c32b08131ac26d9acafe5e9bd2a108bea3a5
ievo <xlievo@live.com>
2019-01-28 08:15:17 +0800

    add: elk-redis

---
+++ elk/elk-docker/README-short.txt
@@ -0,0 +1,1 @@
+Collect, search and visualise log data with ELK (Elasticsearch 6.5.1, Logstash 6.5.1, Kibana 6.5.1).
---
+++ elk/elk-docker/redis-password.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+p=`openssl rand -base64 8 | cut -c 1-8`
+sed -i '/^# requirepass/arequirepass '$p /root/redis-stable/redis.conf
+sed -i '/^		# password/a		password => '\"$p\" /etc/logstash/conf.d/logstash-redis.conf
