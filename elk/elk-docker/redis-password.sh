db828af1eec49b0647201dfd0ab7b792f6e9ebad
ievo <xlievo@live.com>
2019-02-10 00:52:18 +0800

    update: redis-password.sh

--- elk/elk-docker/redis-password.sh
+++ elk/elk-docker/redis-password.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-p=`openssl rand -base64 8 | cut -c 1-8`
-sed -i 's/^# requirepass $p/requirepass '$p'/' /root/redis-stable/redis.conf
-sed -i 's/^		# password => $p/		password => '\"$p\"'/' /etc/logstash/conf.d/logstash-redis.conf
+p=`openssl rand -hex 8 | cut -c 1-8`
+sed -i 's/^# requirepass $p/requirepass '$p'/g' /root/redis-stable/redis.conf
+sed -i 's/^		# password => $p/		password => '\"$p\"'/g' /etc/logstash/conf.d/logstash-redis.conf
