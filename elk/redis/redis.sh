5ad5cb3636b2e72c0f6f7f8923e1d53f0c98de78
ievo <xlievo@live.com>
2019-02-10 22:11:20 +0800

    update: elk

--- elk/redis/redis.sh
+++ elk/redis/redis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 REDIS_HOME = "/opt/redis"
+LOGSTASH_PATH_CONF = "/etc/logstash"
 
-sh $REDIS_HOME/redis-password.sh
+p=`openssl rand -hex 8 | cut -c 1-8`
+sed -i 's/^# requirepass $p/requirepass '$p'/g' $REDIS_HOME/redis.conf
+sed -i 's/^		# password => $p/		password => '\"$p\"'/g' $LOGSTASH_PATH_CONF/conf.d/logstash-redis.conf
+
 $REDIS_HOME/src/redis-server $REDIS_HOME/redis.conf
 sed -n 507,507p $REDIS_HOME/redis.conf
