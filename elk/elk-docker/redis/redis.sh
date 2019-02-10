aa33ad2f90455d4ad4030440289b2d5e4627bd83
ievo <xlievo@live.com>
2019-02-10 22:40:05 +0800

    update: elk

--- elk/elk-docker/redis/redis.sh
+++ elk/elk-docker/redis/redis.sh
@@ -5,7 +5,7 @@ LOGSTASH_PATH_CONF = "/etc/logstash"
 
 p=`openssl rand -hex 8 | cut -c 1-8`
 sed -i 's/^# requirepass $p/requirepass '$p'/g' $REDIS_HOME/redis.conf
-sed -i 's/^		# password => $p/		password => '\"$p\"'/g' $LOGSTASH_PATH_CONF/conf.d/logstash-redis.conf
+sed -i 's/^		# password => $p/password => '\"$p\"'/g' $LOGSTASH_PATH_CONF/conf.d/logstash-redis.conf
 
 $REDIS_HOME/src/redis-server $REDIS_HOME/redis.conf
 sed -n 507,507p $REDIS_HOME/redis.conf
