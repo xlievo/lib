1d88c6c57cd5c32ac101b2c0ed5e7acd87b847f7
ievo <xlievo@live.com>
2019-02-25 17:17:22 +0800

    update: postgresql

--- postgres/10/postgres.sh
+++ postgres/10/postgres.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-sed -i 's/^log_rotation_age = 1d/log_rotation_age = 1d/g' $PGDATA/postgresql.conf
-sed -i 's/^log_rotation_size = 100MB/log_rotation_size = 100MB/g' $PGDATA/postgresql.conf
-sed -i 's/^log_lock_waits = on/log_lock_waits = on/g' $PGDATA/postgresql.conf
-sed -i 's/^log_statement = 'none'/log_statement = 'ddl'/g' $PGDATA/postgresql.conf
-sed -i 's/^log_duration = off/log_duration = on/g' $PGDATA/postgresql.conf
-sed -i 's/^log_min_duration_statement = -1/log_min_duration_statement = 60/g' $PGDATA/postgresql.conf
-sed -i 's/^log_timezone = 'UTC'/log_timezone = 'Asia/Shanghai'/g' $PGDATA/postgresql.conf
+set CONF = $PGDATA/postgresql.conf
 
-RUN ENV TZ=Asia/Shanghai
+sed -i 's/^#log_destination = '\''stderr'\''/log_destination = '\''stderr'\''/g' $CONF
+sed -i 's/^#logging_collector = off/logging_collector = on/g' $CONF
+sed -i 's/^#log_directory = '\''log'\''/log_directory = '\''log'\''/g' $CONF
+sed -i 's/^#log_filename/log_filename/g' $CONF
+sed -i 's/^#log_rotation_age = 1d/log_rotation_age = 1d/g' $CONF
+sed -i 's/^#log_rotation_size = 10MB/log_rotation_size = 100MB/g' $CONF
+sed -i 's/^#log_error_verbosity = default/log_error_verbosity = verbose/g' $CONF
+
+sed -i 's/^#log_statement = '\''none'\''/log_statement = '\''mod'\''/g' $CONF
+
+sed -i 's/^#log_lock_waits = off/log_lock_waits = on/g' $CONF
+
+sed -i 's/^#log_min_duration_statement = -1/log_min_duration_statement = 1000/g' $CONF
+sed -i 's/^#log_checkpoints = off/log_checkpoints = on/g' $CONF
+sed -i 's/^#log_duration = off/log_duration = on/g' $CONF
+
+sed -i 's/^#log_min_messages = warning/log_min_messages = warning/g' $CONF
+sed -i 's/^#log_min_error_statement = error/log_min_error_statement = error/g' $CONF
+
+sed -i 's/^#log_line_prefix = '\''%m \[%p\] '\''/log_line_prefix = '\''%m \[%p\] %q%u@%d '\''/g' $CONF
+
+sed -i 's/^log_timezone = '\''UTC'\''/log_timezone = '\''Asia/Shanghai'\''/g' $CONF
