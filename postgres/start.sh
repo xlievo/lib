bc2b587ea6427c1ab8ea81702df56cf998b0d8af
ievo <xlievo@live.com>
2019-03-12 03:42:37 +0800

    update: postgres master/slave

--- postgres/start.sh
+++ postgres/start.sh
@@ -4,23 +4,23 @@ DATA=/var/lib/postgresql/data
 CONF=$DATA/postgresql.conf
 CONFEX=$DATA/postgresql.ex.conf
 
-logging_collector_key=`cat $CONF | grep logging_collector | awk -F'=' '{ print $1 }' | sed s/[[:space:]]//g`
-log_directory_key=`cat $CONF | grep log_directory | awk -F'=' '{ print $1 }' | sed s/[[:space:]]//g`
-log_day_key=`cat $CONFEX | grep log_day | awk -F'=' '{ print $1 }' | sed s/[[:space:]]//g`
-back_day_key=`cat $CONFEX | grep back_day | awk -F'=' '{ print $1 }' | sed s/[[:space:]]//g`
-back_auto_key=`cat $CONFEX | grep back_auto | awk -F'=' '{ print $1 }' | sed s/[[:space:]]//g`
-back_directory_key=`cat $CONFEX | grep back_directory | awk -F'=' '{ print $1 }' | sed s/[[:space:]]//g`
-back_time_key=`cat $CONFEX | grep back_time | awk -F'=' '{ print $1 }' | sed s/[[:space:]]//g`
+logging_collector_key=`cat $CONF | grep logging_collector | awk -F'=' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+log_directory_key=`cat $CONF | grep log_directory | awk -F'=' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+log_day_key=`cat $CONFEX | grep log_day | awk -F'=' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_day_key=`cat $CONFEX | grep back_day | awk -F'=' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_auto_key=`cat $CONFEX | grep back_auto | awk -F'=' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_directory_key=`cat $CONFEX | grep back_directory | awk -F'=' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_time_key=`cat $CONFEX | grep back_time | awk -F'=' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
 
-logging_collector=`cat $CONF | grep logging_collector | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/[[:space:]]//g`
-log_directory=`cat $CONF | grep log_directory | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/[[:space:]\']//g`
-log_day=`cat $CONFEX | grep log_day | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/[[:space:]]//g`
-back_day=`cat $CONFEX | grep back_day | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/[[:space:]]//g`
-back_auto=`cat $CONFEX | grep back_auto | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/[[:space:]]//g`
-back_directory=`cat $CONFEX | grep back_directory | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/[[:space:]\']//g`
-back_time=`cat $CONFEX | grep back_time | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/[[:space:]]//g`
+logging_collector=`cat $CONF | grep logging_collector | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+log_directory=`cat $CONF | grep log_directory | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+log_day=`cat $CONFEX | grep log_day | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_day=`cat $CONFEX | grep back_day | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_auto=`cat $CONFEX | grep back_auto | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_directory=`cat $CONFEX | grep back_directory | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
+back_time=`cat $CONFEX | grep back_time | awk -F'=' '{ print $2 }' | awk -F'#' '{ print $1 }' | sed s/^[[:space:]\']*//g | sed s/[[:space:]\']*$//g`
 
-echo $HOSTNAME postgresql.ex.conf: log_day=$log_day back_auto=$back_auto back_directory=$back_directory back_time=$back_time back_day=$back_day
+echo $HOSTNAME postgresql.ex.conf: log_day=$log_day back_auto=$back_auto back_directory=$back_directory back_time="$back_time" back_day=$back_day
 
 cron_pg=/var/spool/cron/crontabs/postgres
 
@@ -29,7 +29,7 @@ crontab -r
 fi
 
 if [[ $back_time_key == \#* ]]; then
-back_time=0
+back_time='0 0 * * *'
 fi
 
 if [[ $log_directory == \/* ]]; then
@@ -60,11 +60,11 @@ fi
 echo "0 0 * * * find "$back_directory" -mtime +"$back_day" -name \"*\" -exec rm -rf {} \;" >> $DATA/conf && crontab $DATA/conf && rm -f $DATA/conf
 fi
 
-if [[ ! $back_auto_key == \#* ]] && [ "on" = $back_auto ] && [ ! -z $back_time ] && [ true = `echo $back_time | grep -E '[^0-9]' >/dev/null && echo false || echo true` ]; then
+if [[ ! $back_auto_key == \#* ]] && [ "on" = $back_auto ] && [ ! -z "$back_time" ]; then
 if [ -f $cron_pg ]; then
 crontab -l >> $DATA/conf
 fi
-echo "0 $back_time * * * pg_dumpall -U $POSTGRES_USER > $back_directory/\$(date -d \"2 second\" +\"\%Y-\%m-\%d-\%H-\%M-\%S\")-"$HOSTNAME"" >> $DATA/conf && crontab $DATA/conf && rm -f $DATA/conf
+echo "$back_time pg_dumpall -U $POSTGRES_USER > $back_directory/\$(date -d \"2 second\" +\"\%Y-\%m-\%d-\%H-\%M-\%S\")-"$HOSTNAME"" >> $DATA/conf && crontab $DATA/conf && rm -f $DATA/conf
 fi
 
 echo "docker exec -it db crontab -u postgres -l" 
