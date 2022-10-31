5cce49ffacd074f31fe0b81d7446261e313448a4
ievo <xlievo@live.com>
2022-11-01 01:14:39 +0800

    update: jenkins-core

--- jenkins-core/install.sh
+++ jenkins-core/install.sh
@@ -14,7 +14,20 @@ chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 
 apt-get update
-apt-get install -y dotnet-sdk-2.2=2.2.203-1
+# wget http://ftp.cn.debian.org/debian/pool/main/i/icu/libicu63_63.1-6+deb10u3_amd64.deb
+# sudo dpkg -i libicu63_63.1-6+deb10u3_amd64.deb
+# apt-get install -y dotnet-sdk-2.2=2.2.203-1
 apt-get install -y dotnet-sdk-3.1
 apt-get install -y dotnet-sdk-5.0
 apt-get clean
+
+
+# apt-get update
+# apt-get install -y curl libunwind8 gettext apt-transport-https sudo
+
+# wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
+# rm packages-microsoft-prod.deb
+
+# sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
+
